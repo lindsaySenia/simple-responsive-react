f11ad6575bef3e587e5e13017a872951531e9775
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2023-04-27 13:51:00 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+
+}
+
 function App () {
 
-    const [count, setCount] = useState(0);
+    
     
     const handleClick = () => {
-        setCount(count + 1);
-    }
+   
+    };
 
     return (
     <div>
         <button onClick={handleClick}>Add Animal</button>
-        <div>Number of animals: {count}</div>
+        
     </div>
     );
 }
